8669d98c4d8525911f25c2aa2f1b12ac61b67875
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2023-02-11 00:20:12 +0300

    Create Precise-Format.java

---
+++ Precise-Format.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Geeks {
+	
+	static void printInFormat(float a, float b) {
+		float result = a/b;
+		
+		System.out.format("%1.3f",result);
+	}
+}
+
+public class GFG {
+
+
+	public static void main (String[] args) {      
+	   Scanner sc = new Scanner(System.in);
+	    int testcases = sc.nextInt();
+	    
+	    while (testcases-- > 0) {
+	    	
+	    	float a = sc.nextFloat();
+	    	float b = sc.nextFloat();
+	    	
+	    	Geeks g = new Geeks();
+	    	
+	    	g.printInFormat(a,b);
+	    	System.out.println();
+	    }
+}
+
+
+}
